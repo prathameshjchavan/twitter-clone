b238dc11cd8a374201da7b5cc18f0e2f94b8e3fc
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-06-07 21:06:05 +0530

    comment fetching

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -27,8 +27,6 @@ function Tweet({
 		refreshComments();
 	}, []);
 
-	console.log(comments);
-
 	return (
 		<div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
 			<div className="flex space-x-3">
@@ -63,7 +61,7 @@ function Tweet({
 			<div className="flex justify-between mt-5">
 				<div className="link">
 					<ChatAlt2Icon className="w-5 h-5" />
-					<p>5</p>
+					<p>{comments.length}</p>
 				</div>
 				<div className="link">
 					<SwitchHorizontalIcon className="w-5 h-5" />
@@ -75,6 +73,39 @@ function Tweet({
 					<UploadIcon className="w-5 h-5" />
 				</div>
 			</div>
+
+			{/* Comment Box Login */}
+
+			{comments?.length > 0 && (
+				<div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-auto border-t border-gray-100 p-5">
+					{comments.map(
+						({ _id, username, comment, profileImg, _createdAt }) => (
+							<div key={_id} className="relative flex space-x-2">
+								<hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+								<img
+									src={profileImg}
+									className="mt-2 h-7 w-7 rounded-full object-cover"
+									alt="Profile Pic"
+								/>
+								<div>
+									<div className="flex items-center space-x-1">
+										<p className="mr-1 font-bold">{username}</p>
+										<p className="hidden text-sm text-gray-500 lg:inline">
+											@{username.replace(/\s+/g, "").toLowerCase()}
+											&nbsp;&middot;
+										</p>
+										<TimeAgo
+											className="text-sm text-gray-500"
+											date={_createdAt}
+										/>
+									</div>
+									<p>{comment}</p>
+								</div>
+							</div>
+						)
+					)}
+				</div>
+			)}
 		</div>
 	);
 }
